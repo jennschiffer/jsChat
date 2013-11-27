e226b92ed34d2fe8427ba366da97b6fd4af559f8
Jenn Schiffer <jenn@pancaketheorem.com>
2013-11-27 00:05:14 -0500

    get basic chat saved on client side working

--- app.js
+++ app.js
@@ -1,16 +1,24 @@
-var app = require('express')(), 
-    server = require('http').createServer(app), 
-    io = require('socket.io').listen(server);
+var express = require('express');
+var app = express();
+
+var server = require('http').createServer(app);
+var io = require('socket.io').listen(server);
 
 server.listen(3000);
+
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', function (req, res) {
   res.sendfile(__dirname + '/views/index.html');
 });
 
 io.sockets.on('connection', function (socket) {
-  socket.emit('news', { hello: 'world' });
-  socket.on('my other event', function (data) {
-    console.log(data);
+    
+  socket.on('send', function (data) { 
+  	console.log(data);
+  	io.sockets.emit('message',data); 
   });
+  
+  socket.on('disconnect', function () { });
+  
 });
