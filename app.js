d155d9cb8d9a2f0a53e68b2366b6b4e26d828754
Jenn Schiffer <jenn@pancaketheorem.com>
2013-11-29 16:57:04 -0500

    sqlite and cookie auth

--- app.js
+++ app.js
@@ -1,24 +1,87 @@
+/*** jsChat app.js ***/
+
+var jsChatAuthenticatedUsers = [];
+
+
+/* sqlite */
+var fs = require('fs');
+var dbFile = 'jschat.db';
+var exists = fs.existsSync(dbFile);
+var sqlite3 = require('sqlite3').verbose();
+var db = new sqlite3.Database(dbFile);
+
+// if db does not exist, create it with a root user
+db.serialize(function(){
+    if(!exists) {
+        db.run('CREATE TABLE Users (nickname TEXT, password TEXT, color TEXT, role TEXT)');
+        db.run('INSERT INTO Users (nickname, password, color, role) VALUES ("root","root","red","rootabega")');
+    }
+});
+
+
+
+/* express */
 var express = require('express');
 var app = express();
 
+app.use(express.bodyParser());
+app.use(express.cookieParser('my secret here'));
+app.use(express.static(__dirname + '/public'));
+
+// index
+app.get('/', function(req, resp) {
+    resp.sendfile(__dirname + '/views/index.html');
+});
+
+// log in
+app.get('/login', function(req, resp) {
+	resp.sendfile(__dirname + '/views/login.html');
+});
+
+// log off
+app.get('/logoff', function(req, resp) {
+	resp.clearCookie('jschat');
+	resp.sendfile(__dirname + '/views/login.html');
+});
+
+// receive login post data
+app.post('/', function(req, resp) {
+
+	db.all('SELECT * FROM Users WHERE nickname="' + req.body.nickname + '" AND password="' + req.body.password + '"', 
+	  function(err, rows) {
+	  	        
+        if ( err) throw err;
+        if ( rows.length == 1 ) {
+       	    var data = {
+	       	    nickname : req.body.nickname,
+	            color : rows[0].color,
+	            authentic : true
+       	    };
+       	    dataJSON = JSON.stringify(data);
+       	    resp.cookie('jschat', dataJSON);
+        } else {
+	        resp.clearCookie('jschat');
+        }
+        resp.sendfile(__dirname + '/views/index.html');
+   });
+});
+
+
+
+/* socket.io */
 var server = require('http').createServer(app);
 var io = require('socket.io').listen(server);
 
 server.listen(3000);
 
-app.use(express.static(__dirname + '/public'));
-
-app.get('/', function (req, res) {
-  res.sendfile(__dirname + '/views/index.html');
-});
-
 io.sockets.on('connection', function (socket) {
-    
-  socket.on('send', function (data) { 
-    var messages = [];
-  	io.sockets.emit('message',data); 
-  });
   
-  socket.on('disconnect', function () { });
+    socket.on('send', function (data) { 
+        if ( data.chat ) {
+            // send messages 
+            io.sockets.emit('message',data);            
+        }
+    });
   
+    socket.on('disconnect', function () { });
 });
--- public/js/jschat.js
+++ public/js/jschat.js
@@ -1,91 +1,123 @@
 window.onload = function() {
 
 	/* TODO LIST 
-	*	- check against a list of nick/passwords
-	*	- ability to change password
-	* 	- different colors for each nickname
-	*	- timestamp for each message 
-	*	- log messages -> [time #color] name: message
-	*	- @nick alerts
-	*	- markdown support
-	*	- show when user leaves room
-	*	- show when chat has disconnected
-	*	- FIND A PLACE TO HOST THIS THING
-	*/
-	
-	var nickname, password;
+	*	- bind alerts to users
+    *    - list users in the room
+    *     - tell when a user leaves
+    *    - logoff button
+    *	 - room topic
+    *    - ability to change password
+    *     - different colors for each nickname
+    *    - timestamp for each message 
+    *    - log messages -> [time #color] name: message
+    *    - @nick alerts
+    *    - markdown support
+    *    - show when user leaves room
+    *    - show when chat has disconnected
+    *    - FIND A PLACE TO HOST THIS THING
+    */
+    
+    var nickname, 
+        password, 
+        color,
+        messageContainer, 
+        textInput, 
+        socket, 
+        banter;
+        
+    var loginForm = document.forms[0];
+    
+    /*** INIT SOCKETS AND CHAT ***/
 
-	var loginForm = document.forms[0];
+    var initjsChat = function() {
+        
+        /*** jsChatroom and its events ***/
+        document.getElementById('jsChat').style.display = 'block';
+        messageContainer = document.getElementById('jsChat-messages');
+        textInput = document.forms[0].message;
+        banter = [];
 
-	loginForm.onsubmit = function() {
-		/* TODO validate login */
-		nickname = loginForm.nickname.value;
-		password = loginForm.password.value;
-		jsChatAuthenticated();
-		return false;
-	}
+        // add keyup event                    
+        textInput.onkeyup = function(e){
+            var key = e.keyCode;
+            var message = {
+                nick: nickname,
+            };
+            
+            if ( key != '13' ) {
+                return;
+            }
+            else {
+                message.text = this.value;
+                socket.emit('send', { chat: message });
+                this.value = "";
+            }
+        };
+        
+        // submit form event
+        document.forms[0].onsubmit = function() {
+            return false;
+        };
 
-	var jsChatAuthenticated = function() {
+    };
+    
+    /*** initialize socket and its events ***/
+    var initSocket = function() {
+        
+        socket = io.connect('http://localhost:3000');
+        
+        socket.on('connect',function() {
+            console.log('welcome to jsChat');
+            initjsChat();
+        });
+        
+        socket.on('message',function(data) {
+            if ( data.chat ) {
+                updateMessageWindow(data.chat);
+            }
+        });
+        
+        socket.on('disconnect',function() {
+            console.log('The client has disconnected!');
+        });
+    };
+    
+    
+    
+    
+    /*** HELPERS ***/
+    
+    var updateMessageWindow = function(chatData) {
+        banter.push(chatData);
+        var banterHTML = '';
+        
+        for ( var i = 0; i < banter.length; i++ ) {
+            banterHTML += '<li><span class="nick" style="color:' + color + '">' + banter[i].nick 
+                            + ':</span> <span class="text">' + banter[i].text + '</span></li>';
+        }
+        messageContainer.innerHTML = banterHTML;
+        messageContainer.scrollTop = messageContainer.scrollHeight;
+    };
+    
+    
+    
+    
+    /*** INIT ***/
+    
+    var init = (function() {
+    
+    	// check for jschat cookies
+    	// it will return name, color, and if authentic
+    	
+    	if ( document.cookie ) {
+	        initSocket();
+	        console.log(document.cookie);
+    	}
+    	else {
+	    	// if no cookies, redirect to login
+	    	document.location = '/login';
+    	}
 
-		loginForm.parentNode.removeChild(loginForm);
-		document.getElementById('jsChat').style.display = 'block';
-
-		/*** jsChatroom and its events ***/
-		var messageContainer = document.getElementById('jsChat-messages');
-		var textInput = document.forms[0].message;
-		var banter = [];
-
-		// add keyup event					
-		textInput.onkeyup = function(e){
-			var key = e.keyCode;
-			var message = {
-				nick: nickname,
-			};
-			
-			if ( key != '13' ) {
-				return;
-			}
-			else {
-				message.text = this.value;
-				socket.emit('send', { chat: message });
-				this.value = "";
-			}
-		};
-		
-		// submit form event
-		document.forms[0].onsubmit = function() {
-			return false;
-		};
-		
-		
-		/*** sockets and its events ***/
-		var socket = io.connect('http://localhost:3000');
-		
-		socket.on('connect',function() {
-			console.log('wilkommen auf jsChat, ' + nickname);
-		});
-		
-		socket.on('message',function(data) {
-			if ( data.chat ) {
-				banter.push(data.chat);
-				var banterHTML = '';
-				
-				for ( var i = 0; i < banter.length; i++ ) {
-					banterHTML += '<li><span class="nick">' + banter[i].nick 
-									+ ':</span> <span class="text">' + banter[i].text + '</span></li>';
-				}
-				messageContainer.innerHTML = banterHTML;
-				messageContainer.scrollTop = messageContainer.scrollHeight;
-			}
-			else if ( data.system ) {
-				console.log(data);
-			}
-		});
-		
-		socket.on('disconnect',function() {
-			console.log('The client has disconnected!');
-		});
-
-	};
-	
+    }());
+    
 };
